d73733f156d438b1c3a40284f3bff43509f37794
Blessen George <blessen.george30@gmail.com>
2024-11-04 16:28:41 +0530

    Bottom tab navigation update

--- App.js
+++ App.js
@@ -6,15 +6,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import ManageExpences from './screens/ManageExpences';
 import RecentExpences from './screens/RecentExpences';
 import AllExpences from './screens/AllExpences';
+import { GlobalStyles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons'
 
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpencesOverview(){
-  return<BottomTabs.Navigator screenOptions={{}}>
-    <BottomTabs.Screen name='RecentExpences' component={RecentExpences} />
-    <BottomTabs.Screen name='AllExpences' component={AllExpences} />
+  return<BottomTabs.Navigator 
+  screenOptions={{headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+  headerTintColor: 'white',
+  tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+  tabBarActiveTintColor: GlobalStyles.colors.accent500}}>
+
+    <BottomTabs.Screen 
+    name='RecentExpences' 
+    component={RecentExpences} options={{
+      title: 'Recent Expences' ,
+      tabBarLabel: 'Recent',
+      tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color}/>
+    }} />
+    <BottomTabs.Screen name='AllExpences' component={AllExpences} options={{
+      title: 'All Expences' ,
+      tabBarLabel: 'All Expences',
+      tabBarIcon: ({color, size}) => <Ionicons name="calendar" size={size} color={color} />
+    }} />
   </BottomTabs.Navigator>
 }
 
